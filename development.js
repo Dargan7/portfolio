be4adc477b60e44a438a59ca889dd4dfb1b12bd1
andrew-fennell <andrew.fennell1999@me.com>
2020-09-14 02:50:26 -0500

    Added Development page. Added Flickity.

---
+++ development.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.main-gallery').flickity({
+        // options
+        cellAlign: 'center',
+        contain: true,
+        wrapAround: true,
+        imagesLoaded: true
+    });
+
+
+});
